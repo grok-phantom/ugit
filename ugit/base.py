1cc29e0303a5ea36133e4575553f50e90c10d019
Lei Chen <grok.phantom@gmail.com>
2020-10-02 13:32:40 +0800

    write-tree: ignore .ugit files

--- ugit/base.py
+++ ugit/base.py
@@ -4,7 +4,11 @@ from . import data
 
 def write_tree(directory='.'):
   for entry in Path(directory).iterdir():
-    if entry.is_file() and not entry.is_symlink():
+    if entry.is_file() and not entry.is_symlink() and not is_ignored(entry):
       print(entry)
     elif entry.is_dir() and not entry.is_symlink():
       write_tree(entry)
+
+
+def is_ignored(path):
+  return '.ugit' in Path(path).parts
