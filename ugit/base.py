65ee239583a23ac02b92a0f0ad9bce1acf868b93
Lei Chen <grok.phantom@gmail.com>
2020-10-02 13:35:33 +0800

    write-tree: hash the files

--- ugit/base.py
+++ ugit/base.py
@@ -5,7 +5,8 @@ from . import data
 def write_tree(directory='.'):
   for entry in Path(directory).iterdir():
     if entry.is_file() and not entry.is_symlink() and not is_ignored(entry):
-      print(entry)
+      with open(entry, 'rb') as f:
+        print(data.hash_object(f.read()), entry)
     elif entry.is_dir() and not entry.is_symlink():
       write_tree(entry)
 
