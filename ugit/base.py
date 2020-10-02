e252c2b6be6fe18ae2ab36b5871a995b8dfe52e8
Lei Chen <grok.phantom@gmail.com>
2020-10-02 13:29:16 +0800

    write-tree: list files

--- ugit/base.py
+++ ugit/base.py
@@ -1,1 +1,10 @@
+from pathlib import Path
 from . import data
+
+
+def write_tree(directory='.'):
+  for entry in Path(directory).iterdir():
+    if entry.is_file() and not entry.is_symlink():
+      print(entry)
+    elif entry.is_dir() and not entry.is_symlink():
+      write_tree(entry)
